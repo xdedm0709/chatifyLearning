1b654a65decf117e0f7342c1b1e5a1dfde753831
xdedm0709 <1346278957@qq.com>
2025-11-27 11:07:36 +0800

    SignUp Page
    注册页面前端完成

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -16,6 +17,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json());
+app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,36 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route, Navigate } from 'react-router';
 import ChatPage from './pages/ChatPage'
 import LoginPage from './pages/LoginPage'
 import SignUpPage from './pages/SignUpPage'
 import { useAuthStore } from './store/useAuthStore';
+import { useEffect } from 'react';
+import PageLoader from './components/PageLoader';
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  const { checkAuth, isCheckingAuth, authUser } = useAuthStore();
 
-  const {authUser, isLoggedIn, login} = useAuthStore();
-  console.log("authUser:", authUser);
-  console.log("isLoggedIn:", isLoggedIn);
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth) return <PageLoader />
+
+
   return (
-    
+
     <div className='min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden'>
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
       <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
 
-      <button onClick={login} className='z-10'>login</button>
-
       <Routes>
-        <Route path="/" element={<ChatPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/" element={authUser ? <ChatPage /> : <Navigate to={"/login"} />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />} />
       </Routes>
+
+      <Toaster />
     </div>
 
   )
